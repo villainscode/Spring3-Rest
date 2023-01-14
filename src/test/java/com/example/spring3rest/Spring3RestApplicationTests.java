c4300fe221772aa32de724f4451d3c332c91ec71
villainscode <villainscode@gmail.com>
2023-01-14 19:35:22 +0900

    Initial commit 테스트

---
+++ src/test/java/com/example/spring3rest/Spring3RestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring3rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring3RestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
